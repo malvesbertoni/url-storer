7a6e5fe66d1bb7bd10d40a5cc5af12eb63499365
malvesbertoni <malvesbertoni@gmail.com>
2020-04-28 20:37:30 -0300

    'addToList' function

--- http/public/scripts.js
+++ http/public/scripts.js
@@ -1,6 +1,6 @@
-const ul = document.querySelector("ul")
-const input = document.querySelector("input")
-const form = document.querySelector('form')
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const form = document.querySelector('form');
 
 async function load() {
     const res = await fetch("http://localhost:3000").then((data) => data.json());
@@ -10,45 +10,56 @@ async function load() {
 
 load();
 
-function addElement({ name, url }) {
-    const li = document.createElement('li')
-    const a = document.createElement("a")
-    const trash = document.createElement("span")
-
-    a.href = url
-    a.innerHTML = name
-    a.target = "_blank"
-
-    trash.innerHTML = "x"
-    trash.onclick = () => removeElement(trash)
-
-    li.append(a)
-    li.append(trash)
-    ul.append(li)
+async function addToList({ name, url }) {
+    const res = await fetch(`http://localhost:3000?name=${name}&url=${url}`).then((data) => data.json());
 }
 
-function removeElement(el) {
-    if (confirm('Tem certeza que deseja deletar?'))
-        el.parentNode.remove()
+function addElement({ name, url }) {
+    const li = document.createElement('li');
+    const a = document.createElement("a");
+    const trash = document.createElement("span");
+
+    a.href = url;
+    a.innerHTML = name;
+    a.target = "_blank";
+
+    trash.innerHTML = "x";
+    trash.onclick = () => removeElement(trash);
+
+    li.append(a);
+    li.append(trash);
+    ul.append(li);
+}
+
+async function removeElement(el) {
+    if (confirm('Tem certeza que deseja deletar?')) {
+        //const name = el.parentNode.innerHTML;
+        //console.log(`innerHTML: ${name}`);
+        
+        el.parentNode.remove();
+        
+        //const res = await fetch(`http://localhost:3000?name=${name}&url=${url}&del=1`).then((data) => data.json());
+    } 
 }
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let { value } = input
+    let { value } = input;
 
     if (!value) 
-        return alert('Preencha o campo')
+        return alert('Preencha o campo');
 
-    const [name, url] = value.split(",")
+    const [name, url] = value.split(", ");
 
     if (!url) 
-        return alert('formate o texto da maneira correta')
+        return alert('formate o texto da maneira correta');
 
     if (!/^http/.test(url)) 
-        return alert("Digite a url da maneira correta")
+        return alert("Digite a url da maneira correta");
+    
+    addToList({ name, url });
+    addElement({ name, url });
 
-    addElement({ name, url })
-
-    input.value = ""
+    input.value = "";
 })
